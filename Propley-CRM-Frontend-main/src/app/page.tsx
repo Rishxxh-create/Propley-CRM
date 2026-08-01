cce2fa72d9d37da0a3e3ec57cba180f58b967c93
Rishabh Sharma <rishabhsh200205@gmail.com>
2026-08-01 21:48:11 +0530

    Update page.tsx

--- Propley-CRM-Frontend-main/src/app/page.tsx
+++ Propley-CRM-Frontend-main/src/app/page.tsx
@@ -8,10 +8,17 @@ export default function Dashboard() {
   useLoadEventStats();
 
   return (
-    <DashboardLayout activePath="/">
+    <>
+      <Script
+      src="https://arvo-telephony-frontend-five.vercel.app/widget.js"
+      data-public-key="pub_DrKNpNEoM84vaTf1-u6y7VPUJuhYmw0E"
+      strategy="afterInteractive"
+    />
+      <DashboardLayout activePath="/">
       <div className="px-4 sm:px-8 py-8 max-w-7xl mx-auto w-full">
         <AdvisorOverview />
       </div>
     </DashboardLayout>
+    </>
   );
 }
